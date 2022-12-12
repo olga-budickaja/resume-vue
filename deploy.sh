6bf8fbc0bcd7482a032a572e454ffdc0e656c793
Olga Budickaja <olgabudickaja@gmail.com>
2022-12-12 06:49:22 +0200

    change dist

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,7 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:olga-budickaja/resume-vue.git master:gh-pages
+# git push -f git@github.com:olga-budickaja/resume-vue.git origin:gh-pages
+
 
 cd -
